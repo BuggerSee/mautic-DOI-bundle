05df0ac1f066a038e3c7eb2c81f57667a4e65f27
Patryk Gruszka <p.gruszka@leuchtfeuer.com>
2025-08-14 17:43:17 +0200

    MauticDoi plugin init

---
+++ Config/config.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'name'        => 'Email Verification and Double Opt-In (DOI) by Leuchtfeuer',
+    'description' => 'Universal email verification for Mautic, including Double Opt-In (DOI)',
+    'version'     => '1.0',
+    'author'      => 'Leuchtfeuer Digital Marketing GmbH',
+];
---
+++ MauticDoiBundle.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MauticPlugin\MauticDoiBundle;
+
+use Mautic\PluginBundle\Bundle\PluginBundleBase;
+
+class MauticDoiBundle extends PluginBundleBase
+{
+}
