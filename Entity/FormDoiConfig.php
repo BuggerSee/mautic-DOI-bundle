18ed907cbf43ecb0e48b673424d28159ce3f904f
Patryk Gruszka <p.gruszka@leuchtfeuer.com>
2025-08-14 17:43:17 +0200

    Form DOI config

---
+++ Entity/FormDoiConfig.php
@@ -0,0 +1,149 @@
+<?php
+
+namespace MauticPlugin\MauticDoiBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Mautic\FormBundle\Entity\Form;
+use Mautic\EmailBundle\Entity\Email;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\Validator\Mapping\ClassMetadata;
+
+#[ORM\Entity(repositoryClass: FormDoiConfigRepository::class)]
+#[ORM\Table(name: 'form_doi_config')]
+#[ORM\UniqueConstraint(name: 'form_id_unique', columns: ['form_id'])]
+class FormDoiConfig
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private ?int $id = null;
+
+    #[ORM\ManyToOne(targetEntity: Form::class)]
+    #[ORM\JoinColumn(name: 'form_id', referencedColumnName: 'id', nullable: false, onDelete: 'CASCADE')]
+    private ?Form $form = null;
+
+    #[ORM\ManyToOne(targetEntity: Email::class)]
+    #[ORM\JoinColumn(name: 'verification_email_id', referencedColumnName: 'id', nullable: false, onDelete: 'RESTRICT')]
+    private ?Email $verificationEmail = null;
+
+    #[ORM\ManyToOne(targetEntity: Email::class)]
+    #[ORM\JoinColumn(name: 'follow_up_email_id', referencedColumnName: 'id', nullable: true, onDelete: 'SET NULL')]
+    private ?Email $followUpEmail = null;
+
+    #[ORM\Column(type: 'string', length: 255, nullable: true)]
+    private ?string $successRedirectUrl = null;
+
+    #[ORM\Column(type: 'string', length: 255, nullable: true)]
+    private ?string $errorRedirectUrl = null;
+
+    #[ORM\Column(type: 'datetime')]
+    private \DateTime $createdAt;
+
+    #[ORM\Column(type: 'datetime')]
+    private \DateTime $updatedAt;
+
+    public function __construct()
+    {
+        $this->createdAt = new \DateTime();
+        $this->updatedAt = new \DateTime();
+    }
+
+    public static function loadValidatorMetadata(ClassMetadata $metadata): void
+    {
+        $metadata->addPropertyConstraint('form', new Assert\NotNull([
+            'message' => 'mautic.form.doi_config.form.required',
+            'groups'  => ['doi_config'],
+        ]));
+        $metadata->addPropertyConstraint('verificationEmail', new Assert\NotNull([
+            'message' => 'mautic.form.doi_config.verification_email.required',
+            'groups'  => ['doi_config'],
+        ]));
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function setForm(Form $form): self
+    {
+        $this->form = $form;
+        return $this;
+    }
+
+    public function getForm(): ?Form
+    {
+        return $this->form;
+    }
+
+    public function setVerificationEmail(Email $email): self
+    {
+        $this->verificationEmail = $email;
+        return $this;
+    }
+
+    public function getVerificationEmail(): ?Email
+    {
+        return $this->verificationEmail;
+    }
+
+    public function setFollowUpEmail(?Email $email): self
+    {
+        $this->followUpEmail = $email;
+        return $this;
+    }
+
+    public function getFollowUpEmail(): ?Email
+    {
+        return $this->followUpEmail;
+    }
+
+    public function setSuccessRedirectUrl(?string $url): self
+    {
+        $this->successRedirectUrl = $url;
+        return $this;
+    }
+
+    public function getSuccessRedirectUrl(): ?string
+    {
+        return $this->successRedirectUrl;
+    }
+
+    public function setErrorRedirectUrl(?string $url): self
+    {
+        $this->errorRedirectUrl = $url;
+        return $this;
+    }
+
+    public function getErrorRedirectUrl(): ?string
+    {
+        return $this->errorRedirectUrl;
+    }
+
+    public function setCreatedAt(\DateTime $createdAt): self
+    {
+        $this->createdAt = $createdAt;
+        return $this;
+    }
+
+    public function getCreatedAt(): \DateTime
+    {
+        return $this->createdAt;
+    }
+
+    public function setUpdatedAt(\DateTime $updatedAt): self
+    {
+        $this->updatedAt = $updatedAt;
+        return $this;
+    }
+
+    public function getUpdatedAt(): \DateTime
+    {
+        return $this->updatedAt;
+    }
+
+    public function convertToArray(): array
+    {
+        return get_object_vars($this);
+    }
+}
---
+++ Entity/FormDoiConfigRepository.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace MauticPlugin\MauticDoiBundle\Entity;
+
+use Mautic\CoreBundle\Entity\CommonRepository;
+
+class FormDoiConfigRepository extends CommonRepository
+{
+
+}
