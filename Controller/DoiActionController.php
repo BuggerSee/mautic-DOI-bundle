b0fb9820466cd073a13d9f78ac95d01d761bfdf0
Patryk Gruszka <p.gruszka@leuchtfeuer.com>
2025-08-20 16:16:15 +0200

    Refactor: phpstan

--- Controller/DoiActionController.php
+++ Controller/DoiActionController.php
@@ -125,100 +125,102 @@ class DoiActionController extends AbstractStandardFormController
         $valid      = $cancelled      = false;
         $formAction = array_key_exists($objectId, $actions) ? $actions[$objectId] : null;
 
-        if (null !== $formAction) {
-            $actionType             = $formAction['type'];
-            $customComponents       = $formModel->getCustomComponents();
-            $formAction['settings'] = $customComponents['actions'][$actionType];
+        if (null === $formAction) {
+            return new JsonResponse(null, Response::HTTP_NOT_FOUND);
+        }
 
-            // ajax only for form fields
-            if (!$actionType
-                || !$request->isXmlHttpRequest()
-                || !$this->security->isGranted(['form:forms:editown', 'form:forms:editother', 'form:forms:create'], 'MATCH_ONE')
-            ) {
-                return $this->modalAccessDenied();
-            }
+        $actionType             = $formAction['type'];
+        $customComponents       = $formModel->getCustomComponents();
+        $formAction['settings'] = $customComponents['actions'][$actionType];
 
-            $form = $this->formFactory->create(ActionType::class, $formAction, [
-                'action'   => $this->generateUrl('mautic_doi_formaction_action', ['objectAction' => 'edit', 'objectId' => $objectId]),
-                'settings' => $formAction['settings'],
-                'formId'   => $formId,
-            ]);
-            $form->get('formId')->setData($formId);
+        // ajax only for form fields
+        if (!$actionType
+            || !$request->isXmlHttpRequest()
+            || !$this->security->isGranted(['form:forms:editown', 'form:forms:editother', 'form:forms:create'], 'MATCH_ONE')
+        ) {
+            return $this->modalAccessDenied();
+        }
 
-            // Check for a submitted form and process it
-            if ('POST' == $method) {
-                if (!$cancelled = $this->isFormCancelled($form)) {
-                    if ($valid = $this->isFormValid($form)) {
-                        // form is valid so process the data
-                        $formData = $form->getData();
-                        // overwrite with updated data
-                        $formAction = array_merge($actions[$objectId], $formData);
-                        if (empty($formAction['name'])) {
-                            // set it to the event default
-                            $formAction['name'] = $this->translator->trans($formAction['settings']['label']);
-                        }
-                        $formDoiActionSessionManager->updateActionInSession($formId, $objectId, $formAction);
+        $form = $this->formFactory->create(ActionType::class, $formAction, [
+            'action'   => $this->generateUrl('mautic_doi_formaction_action', ['objectAction' => 'edit', 'objectId' => $objectId]),
+            'settings' => $formAction['settings'],
+            'formId'   => $formId,
+        ]);
+        $form->get('formId')->setData($formId);
 
-                        // generate HTML for the field
-                        $keyId = $objectId;
+        // Check for a submitted form and process it
+        if ('POST' == $method) {
+            if (!$cancelled = $this->isFormCancelled($form)) {
+                if ($valid = $this->isFormValid($form)) {
+                    // form is valid so process the data
+                    $formData = $form->getData();
+                    // overwrite with updated data
+                    $formAction = array_merge($actions[$objectId], $formData);
+                    if (empty($formAction['name'])) {
+                        // set it to the event default
+                        $formAction['name'] = $this->translator->trans($formAction['settings']['label']);
+                    }
+                    $formDoiActionSessionManager->updateActionInSession($formId, $objectId, $formAction);
 
-                        // take note if this is a submit button or not
-                        if ('button' == $actionType) {
-                            $submits = $request->getSession()->get('mautic.formactions.submits', []);
-                            if ('submit' == $formAction['properties']['type'] && !in_array($keyId, $submits)) {
-                                // button type updated to submit
-                                $submits[] = $keyId;
-                                $request->getSession()->set('mautic.formactions.submits', $submits);
-                            } elseif ('submit' != $formAction['properties']['type'] && in_array($keyId, $submits)) {
-                                // button type updated to something other than submit
-                                $key = array_search($keyId, $submits);
-                                unset($submits[$key]);
-                                $request->getSession()->set('mautic.formactions.submits', $submits);
-                            }
+                    // generate HTML for the field
+                    $keyId = $objectId;
+
+                    // take note if this is a submit button or not
+                    if ('button' == $actionType) {
+                        $submits = $request->getSession()->get('mautic.formactions.submits', []);
+                        if ('submit' == $formAction['properties']['type'] && !in_array($keyId, $submits)) {
+                            // button type updated to submit
+                            $submits[] = $keyId;
+                            $request->getSession()->set('mautic.formactions.submits', $submits);
+                        } elseif ('submit' != $formAction['properties']['type'] && in_array($keyId, $submits)) {
+                            // button type updated to something other than submit
+                            $key = array_search($keyId, $submits);
+                            unset($submits[$key]);
+                            $request->getSession()->set('mautic.formactions.submits', $submits);
                         }
                     }
                 }
             }
+        }
 
-            $result     = $this->determineViewParamsAndModalClosure($actionType, $cancelled, $valid, $form, $formAction);
-            $viewParams = $result['viewParams'];
-            $closeModal = $result['closeModal'];
+        $result     = $this->determineViewParamsAndModalClosure($actionType, $cancelled, $valid, $form, $formAction);
+        $viewParams = $result['viewParams'];
+        $closeModal = $result['closeModal'];
 
-            $passthroughVars = [
-                'mauticContent' => 'formDoiAction',
-                'route'         => false,
-            ];
+        $passthroughVars = [
+            'mauticContent' => 'formDoiAction',
+            'route'         => false,
+        ];
 
-            if (!empty($keyId)) {
-                $passthroughVars['actionId'] = $keyId;
+        if (!empty($keyId)) {
+            $passthroughVars['actionId'] = $keyId;
 
-                // prevent undefined errors
-                $entity     = new FormDoiAction();
-                $blank      = $entity->convertToArray();
-                $formAction = array_merge($blank, $formAction);
-                $template   = (!empty($formAction['settings']['template'])) ? $formAction['settings']['template'] :
-                    '@MauticDoi/Action/_generic.html.twig';
-                $passthroughVars['actionHtml'] = $this->renderView($template, [
-                    'inForm' => true,
-                    'action' => $formAction,
-                    'id'     => $keyId,
-                    'formId' => $formId,
-                ]);
-            }
-
-            if ($closeModal) {
-                // just close the modal
-                $passthroughVars['closeModal'] = 1;
-
-                return new JsonResponse($passthroughVars);
-            }
-
-            return $this->ajaxAction($request, [
-                'contentTemplate' => '@MauticDoi/Builder/'.$viewParams['tmpl'].'.html.twig',
-                'viewParameters'  => $viewParams,
-                'passthroughVars' => $passthroughVars,
+            // prevent undefined errors
+            $entity     = new FormDoiAction();
+            $blank      = $entity->convertToArray();
+            $formAction = array_merge($blank, $formAction);
+            $template   = (!empty($formAction['settings']['template'])) ? $formAction['settings']['template'] :
+                '@MauticDoi/Action/_generic.html.twig';
+            $passthroughVars['actionHtml'] = $this->renderView($template, [
+                'inForm' => true,
+                'action' => $formAction,
+                'id'     => $keyId,
+                'formId' => $formId,
             ]);
         }
+
+        if ($closeModal) {
+            // just close the modal
+            $passthroughVars['closeModal'] = 1;
+
+            return new JsonResponse($passthroughVars);
+        }
+
+        return $this->ajaxAction($request, [
+            'contentTemplate' => '@MauticDoi/Builder/'.$viewParams['tmpl'].'.html.twig',
+            'viewParameters'  => $viewParams,
+            'passthroughVars' => $passthroughVars,
+        ]);
     }
 
     public function deleteAction(Request $request, int $objectId, FormDoiActionSessionManager $formDoiActionSessionManager): JsonResponse
@@ -264,6 +266,11 @@ class DoiActionController extends AbstractStandardFormController
         return 'mautic.plugin.doi.doi_action_manager';
     }
 
+
+    /**
+     * @param array<string, mixed> $formAction
+     * @return array<string, mixed>
+     */
     private function determineViewParamsAndModalClosure(string $actionType, bool $cancelled, bool $valid, FormInterface $form, array $formAction): array
     {
         $viewParams = ['type' => $actionType];
