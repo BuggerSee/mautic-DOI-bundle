21e85aa0170fd7eff8e47555c377c69b646a6fa3
Patryk Gruszka <p.gruszka@leuchtfeuer.com>
2025-11-25 15:00:59 +0100

    [MTC-9256] fix actions template

--- Controller/DoiActionController.php
+++ Controller/DoiActionController.php
@@ -89,7 +89,7 @@ class DoiActionController extends AbstractStandardFormController
             $blank      = $entity->convertToArray();
             $formAction = array_merge($blank, $formAction);
 
-            $template                      = $formAction['settings']['template'] ?? '@LeuchtfeuerDoi/Action/_generic.html.twig';
+            $template                      = '@LeuchtfeuerDoi/Action/_generic.html.twig';
             $passthroughVars['actionId']   = $keyId;
             $passthroughVars['actionHtml'] = $this->renderView($template, [
                 'inForm' => true,
@@ -201,8 +201,7 @@ class DoiActionController extends AbstractStandardFormController
             $entity     = new FormDoiAction();
             $blank      = $entity->convertToArray();
             $formAction = array_merge($blank, $formAction);
-            $template   = (!empty($formAction['settings']['template'])) ? $formAction['settings']['template'] :
-                '@LeuchtfeuerDoi/Action/_generic.html.twig';
+            $template   = '@LeuchtfeuerDoi/Action/_generic.html.twig';
             $passthroughVars['actionHtml'] = $this->renderView($template, [
                 'inForm' => true,
                 'action' => $formAction,
