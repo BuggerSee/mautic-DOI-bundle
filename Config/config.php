ce7144e9a78c9548052d5757e4756173ef959bce
Madlen <m.friedrich@leuchtfeuer.com>
2026-06-04 10:43:14 +0200

    Bump version

--- Config/config.php
+++ Config/config.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 return [
     'name'        => 'Email Verification and Double Opt-In (DOI) by Leuchtfeuer',
     'description' => 'Universal email verification for Mautic, including Double Opt-In (DOI)',
-    'version'     => '5.1.1',
+    'version'     => '5.2.0',
     'author'      => 'Leuchtfeuer Digital Marketing GmbH',
     'routes'      => [
         'main' => [
