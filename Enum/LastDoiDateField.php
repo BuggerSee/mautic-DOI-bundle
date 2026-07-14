54d44826749d171c70411ea8bb762183225b5767
Lukas Drahy <lukas@drahy.net>
2026-07-14 14:28:19 +0200

    MTC-11209 M7 Add Custom Field "Last DOI Date"

---
+++ Enum/LastDoiDateField.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MauticPlugin\LeuchtfeuerDoiBundle\Enum;
+
+final class LastDoiDateField
+{
+    public const ALIAS = 'last_doi_date';
+
+    public const LABEL_TRANSLATION_KEY = 'mautic.plugin.doi.contact_field.last_doi_date';
+
+    private function __construct()
+    {
+    }
+}
--- EventListener/PluginInstallSubscriber.php
+++ EventListener/PluginInstallSubscriber.php
@@ -6,7 +6,9 @@ namespace MauticPlugin\LeuchtfeuerDoiBundle\EventListener;
 
 use Mautic\PluginBundle\Bundle\PluginDatabase;
 use Mautic\PluginBundle\Event\PluginInstallEvent;
+use Mautic\PluginBundle\Event\PluginUpdateEvent;
 use Mautic\PluginBundle\PluginEvents;
+use MauticPlugin\LeuchtfeuerDoiBundle\Service\LastDoiDateFieldInstaller;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
 class PluginInstallSubscriber implements EventSubscriberInterface
@@ -16,8 +18,10 @@ class PluginInstallSubscriber implements EventSubscriberInterface
      */
     private const PLUGIN_NAME = 'Email Verification and Double Opt-In (DOI) by Leuchtfeuer';
 
-    public function __construct(private readonly PluginDatabase $pluginDatabase)
-    {
+    public function __construct(
+        private readonly PluginDatabase $pluginDatabase,
+        private readonly LastDoiDateFieldInstaller $lastDoiDateFieldInstaller,
+    ) {
     }
 
     public function onInstall(PluginInstallEvent $event): void
@@ -29,8 +33,19 @@ class PluginInstallSubscriber implements EventSubscriberInterface
         // Run DB migrations instead of automatic schema update
         $this->pluginDatabase->onPluginUpdate($event->getPlugin());
 
+        $this->lastDoiDateFieldInstaller->installIfMissing();
+
         // Prevent core PluginSubscriber from trying to create schema
         $event->stopPropagation();
+    }
+
+    public function onUpdate(PluginUpdateEvent $event): void
+    {
+        if (!$event->checkContext(self::PLUGIN_NAME)) {
+            return;
+        }
+
+        $this->lastDoiDateFieldInstaller->installIfMissing();
     }
 
     /**
@@ -40,6 +55,7 @@ class PluginInstallSubscriber implements EventSubscriberInterface
     {
         return [
             PluginEvents::ON_PLUGIN_INSTALL => ['onInstall', 10],
+            PluginEvents::ON_PLUGIN_UPDATE  => ['onUpdate', 0],
         ];
     }
 }
